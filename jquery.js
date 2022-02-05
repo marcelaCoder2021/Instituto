3671111f3faeb889fac3ca1f4016c7229165a928
Marcela Garfunkel <arbolgarfunkel@gmail.com>
2022-02-05 10:54:21 -0300

    efecto post it

--- jquery.js
+++ jquery.js
@@ -1,1 +1,18 @@
-//menuNavegacion
+//animacionPostIt  
+$(()=>{  //funcion ready
+  $(".postIt").on("click",function(){//funcion on
+  $(".postIt img").addClass("animacionPostIt")//agrego la clase 
+    $(".animacionPostIt").animate({ //animacion
+      left:'250px',
+      height:'+=50px',
+      width:'+=50px',
+    },"slow");
+    $(".animacionPostIt").animate({ //animacion
+      right:'250px',
+      height:'-=40px',
+      width:'-=40px',
+    },"slow");
+    e.preventDefault();
+  });   
+})
+    
